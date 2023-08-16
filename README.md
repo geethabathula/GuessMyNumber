864c76f7a1f53dce58d6aad3caaba7431a229335
geethabathula <bgeethasupriya.99@gmail.com>
2023-08-16 20:07:43 +0530

    Upgraded the game added more functionalities

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This a game developed basing on basic DOM Manipulation
+This a game developed on Java Script DOM Manipulation
 
 Things I Learned :
 
@@ -6,7 +6,7 @@ Things I Learned :
 2. CSS var method
 3. Basic JavaScript
 
-Challenges Faced "
+Challenges Faced
 
 1. favicon.ico error
 
@@ -15,3 +15,21 @@ http://127.0.0.1:5500/favicon.ico
 [HTTP/1.1 404 Not Found 0ms]
 Resolution :
 https://stackoverflow.com/questions/1321878/how-to-prevent-favicon-ico-requests#
+
+2. Footer
+
+I encountered an issue with my footer: when there's less content, the footer doesn't remain at the bottom; however, with more content, the footer sticks to the bottom.
+
+To address this, I've adopted the sticky footer approach. I've set the parent container to have a minimum height of 100vh, which ensures that it occupies at least the full viewport height. Additionally, I've applied the following styles to the footer:
+
+css
+
+footer {
+position: sticky;
+top: 100%;
+/_ ...other styles... _/
+}
+
+The position: sticky; property makes the footer behave as a sticky element. When content pushes the footer beyond the viewport, it sticks to the bottom of the container. The top: 100%; rule ensures that the footer initially starts at the bottom of the container.
+
+Overall, my approach aims to maintain a consistent layout: the footer remains at the bottom regardless of the content's height. This provides a clean and predictable user experience across various content scenarios.
--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,113 @@
 //Guess Number Game
 // Working Version-2.0
+
 let getNumberBox = document.querySelector('#number-box');
+let initialNumberBox = getNumberBox.textContent;
 let getGuessCount = document.querySelector('.guess-count');
 let getSectionMessage = document.querySelector('.section-message');
+let initialSectionMessage = getSectionMessage.textContent;
 let getSectionGuessCount = document.querySelector('.section-guess-count');
+let inputNumber = document.querySelector('.section-number');
+let initialInputNumber = inputNumber.textContent;
+let checkButton = document.querySelector('.section-check-button');
+let resetButton = document.querySelector('.section-reset-button');
 
+let createh6 = document.createElement('h4');
+let getTitleContainter = document.querySelector('.titlecontainer');
+createh6.classList.add('title-header-6');
+createh6.textContent = 'The ✅ Number Is 👇';
+createh6.style.fontSize = "17px";
+createh6.style.marginTop = "35px";
+createh6.style.marginBottom = "5px";
+
+let getSection = document.querySelectorAll('#section');
+let getClearBtn = document.querySelector('.clearbutton');
+getClearBtn.style.display = "none";
 
 let countGuess = 3;
-let randomNum = Math.floor(Math.random() * 10) + 1;
+let randomNumGenerator = () => Math.floor(Math.random() * 10) + 1;
 
-let attempt = 0;
+let randomNum = randomNumGenerator();
 
 const displayMessage = function (message) {
     getSectionMessage.textContent = message;
 };
 
-
-document.querySelector('.section-check-button').addEventListener('click', guessGame);
 //Game Functionality
 function guessGame() {
-    let usrInput = Number(document.querySelector('.section-number').value);
-
-    if (usrInput === 0) {
-        displayMessage(`⛔️ Please Enter a number`);
-        return;
-    }
-    attempt++;
-
-    if (usrInput === randomNum) {
-        displayMessage(`Hurray...🤩 You won in ${attempt} attempt/s.`);
-        getSectionMessage.style.color = 'var(--win-color)';
-        getNumberBox.textContent = randomNum;
-        getSectionGuessCount.textContent = "";
-    } else {
-        countGuess--;
-        if (countGuess >= 0) {
-            getGuessCount.textContent = countGuess;
-            getGuessCount.style.color = 'var(--count-color)';
+    let usrInput = Number(inputNumber.value);
+    if (usrInput > 0 && usrInput <= 10) {
+        if (usrInput === 0) {
+            displayMessage(`⛔️ Please Enter a number`);
+            return;
         }
 
 
-        if (countGuess === 0) {
-            displayMessage(`Sorry🙁 you ran out of Guesses. Better Luck Next Time`);
-            getSectionMessage.style.color = 'var(--lost-color)';
+        if (usrInput === randomNum) {
+            displayMessage(`Hurray...🤩 You won.`);
+            getSectionMessage.style.color = 'var(--win-color)';
             getNumberBox.textContent = randomNum;
-            getSectionGuessCount.textContent = "";
-        } else if (usrInput > randomNum) {
-            displayMessage(`Try a lower number.`);
-        } else if (usrInput < randomNum) {
-            displayMessage(`Try a higher number.`);
+            getSectionGuessCount.style.display = "none";
+            getTitleContainter.appendChild(createh6);
+            getClearBtn.style.display = "block";
+            inputNumber.disabled = "true";
+            checkButton.disabled = "true";
+            resetButton.disabled = "true";
+
+        } else {
+            countGuess--;
+            if (countGuess >= 0) {
+                getGuessCount.textContent = countGuess;
+                getGuessCount.style.color = 'var(--count-color)';
+            }
+
+            if (countGuess === 0) {
+                displayMessage(`Sorry🙁 you ran out of Guesses. Better Luck Next Time`);
+                getSectionMessage.style.color = 'var(--lost-color)';
+                getNumberBox.textContent = randomNum;
+                getSectionGuessCount.style.display = "none";
+                getTitleContainter.appendChild(createh6);
+                getClearBtn.style.display = "block";
+                inputNumber.disabled = "true";
+                checkButton.disabled = "true";
+                resetButton.disabled = "true";
+
+            } else if (usrInput > randomNum) {
+                displayMessage(`Try a lower number.`);
+            } else if (usrInput < randomNum) {
+                displayMessage(`Try a higher number.`);
+            }
         }
+
+    } else {
+        displayMessage(`⛔ Enter Numbers between 1 to 10 only`)
     }
+
+
 }
+
+checkButton.addEventListener('click', guessGame);
+
 //Reset Functionality
-document.querySelector('.section-reset-button').addEventListener('click', function () {
+resetButton.addEventListener('click', function () {
     document.querySelector('.section-number').value = "";
 });
+
+//Restart Functionality
+getClearBtn.addEventListener('click', restart);
+function restart() {
+    getSectionMessage.textContent = initialSectionMessage;
+    getSectionMessage.style.color = "var(--black)";
+    inputNumber.removeAttribute("disabled");
+    checkButton.removeAttribute("disabled");
+    resetButton.removeAttribute("disabled");
+    randomNum = randomNumGenerator();
+    countGuess = 3;
+    getSectionGuessCount.style.display = "block";
+    document.querySelector('.section-number').value = "";
+    getNumberBox.textContent = initialNumberBox;
+    getGuessCount.textContent = countGuess;
+    getGuessCount.style.color = 'var(--count-color)';
+    createh6.textContent = '';
+    getClearBtn.style.display = "none";
+}
